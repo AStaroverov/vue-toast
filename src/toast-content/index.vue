d1dde7a8a6b382365cda08f8df9843b6f60beafc
matewiszt <matezoltan.wiszt@gmail.com>
2018-04-03 14:29:44 +0200

    Add toast-content component
    Add toast-content component to make the toast content configurable

---
+++ src/toast-content/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <span>message</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            message: {
+                required: true
+            }
+        }
+    }
+</script>
